cdaf319a178ba016caa2a56f3087fe7e22807f52
Jeppe Johansen <jepp2078@gmail.com>
2022-06-24 14:11:50 +0200

    Update lock.sh

--- .local/bin/scripts/lock.sh
+++ .local/bin/scripts/lock.sh
@@ -1,14 +1,16 @@
 #!/bin/bash
 
-swaylock \
+swaylock -f \
 	--screenshots \
 	--clock \
 	--indicator \
-	--indicator-radius 100 \
-	--indicator-thickness 7 \
-	--effect-blur 7x5 \
-	--ring-color bb00cc \
+	--indicator-radius 280 \
+	--indicator-thickness 5 \
+	--effect-blur 2x10 \
+	--ring-color BED4D6 \
 	--key-hl-color 880033 \
 	--line-color 00000000 \
-	--inside-color 00000088 \
+	--inside-color F24BA733 \
 	--separator-color 00000000
+
+systemctl suspend
